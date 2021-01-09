daaffef90912f4224792a2d64578c668c9210860
Sir-kinuthia-001 <33373880+Sir-kinuthia-001@users.noreply.github.com>
2021-01-09 20:10:26 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+//calll modal addEventListener
+const addModal = document.querySelector('.add-modal');
+const btnadd = document.querySelector('.btn-add');
+//click add user button
+btnadd.addEventListener('click', ()=>{
+  addModal.classList.add('modal-show');
+});
+//user click anywhere outside the modal
+window.addEventListener('click',e=>{
+  if(e.target ===addModal){
+    addModal.classList.remove('modal-show');
+  }
+  //console.log(e.target, addModal);
+
+});
